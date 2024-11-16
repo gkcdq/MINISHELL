9a38f1680db2b4bb92f096b537fb1eee32c58abe
Thomas Milin <tmilin@made-f0Ar3s5.clusters.42paris.fr>
2024-11-16 14:15:57 +0100

    42

--- minishell/minishell.h
+++ minishell/minishell.h
@@ -1,1 +1,19 @@
-#
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "libft/libft.h"       // Bibliothèque personnelle
+# include <sys/types.h>         // pid_t, etc.
+# include <sys/wait.h>          // wait, waitpid, etc.
+# include <signal.h>            // signal, sigaction, kill, etc.
+# include <dirent.h>            // opendir, readdir, closedir, etc.
+# include <errno.h>             // errno, perror, etc.
+# include <sys/stat.h>          // stat, lstat, fstat
+# include <readline/readline.h> // readline, add_history, etc.
+# include <readline/history.h>  // Historique des commandes
+# include <termios.h>           // tcsetattr, tcgetattr
+# include <curses.h>            // tputs, tgoto, etc.    
+
+
+
+
+#endif
